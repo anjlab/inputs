b102d9a915639e11d5f726aea27ddaa2f1eb19fb
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2014-12-05 13:13:45 +0300

    inital commit

---
+++ assets/javascripts/inputs.js
@@ -0,0 +1,26 @@
++function ($) {
+  'use strict';
+
+  function setInputValue() {
+    var val = $(this).val();
+    val ? $(this).attr('value', val) : $(this).removeAttr('value');
+  }
+
+  function setTextareaValue() {
+    $(this).text($(this).val());
+  }
+
+  $(document).on('change', "input[type='text'], input[type='email'], input[type='password']", setInputValue);
+  $(document).on('change', "textarea", setTextareaValue);
+
+  $.fn.refresh = setInputValue;
+
+  $(window).on('load', function() {
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+    if (isFirefox) {
+      $("input[type='text']:not([value]), input[type='email']:not([value]), input[type='password']:not([value])")
+          .filter(function() { return $(this).val();}).refresh();
+    }
+  });
+
+}(jQuery);
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var config = {
+  jsPath: './assets/javascripts',
+  lessPath: './assets/stylesheets',
+  bowerDir: './bower_components'
+}
+
+gulp.task('bower', function() { 
+  return $.bower().pipe(gulp.dest(config.bowerDir)) ;
+});
+
+gulp.task('css', function() { 
+  return gulp.src(config.lessPath + '/*-inputs.less')
+          .pipe($.less({
+             paths: [
+                 config.lessPath,
+                 config.bowerDir + '/bootstrap/less'
+             ]
+          }) )
+         .pipe($.autoprefixer('last 2 version'))
+          .pipe(gulp.dest('./dist/css'))
+         .pipe(reload({stream:true}))
+         .pipe($.minifyCss())
+         .pipe($.rename({suffix: '-min'}))
+         .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('js', function() {
+  return gulp.src(config.jsPath + '/inputs.js')
+         .pipe(gulp.dest('./dist/js'))
+         .pipe(reload({stream:true}))
+         .pipe($.uglify())
+         .pipe($.rename({suffix: '-min'}))
+         .pipe(gulp.dest('./dist/js'));
+});
+
+ gulp.task('watch', function() {
+   gulp.watch(config.lessPath + '/**/*.less', ['css']); 
+  gulp.watch(config.jsPath + '/**/*.js', ['js']); 
+  gulp.watch('./dist/docs/*.html', reload);
+});
+
+gulp.task('serve', ['watch'], function() {
+  browserSync({
+    server: {
+      baseDir: ['dist/docs', 'dist']
+    }
+  });
+});
+
+gulp.task('default', ['bower', 'css', 'js']);
